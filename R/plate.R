d69dcfb4a9f264cb64404a257a85f93054792281
randomchars42 <randomchars42@users.noreply.github.com>
2017-10-01 14:10:27 +0200

    add checks on params for plate_read, add documentation for plate_read

--- R/plate.R
+++ R/plate.R
@@ -65,28 +65,53 @@
 #'   1 \tab NameH12 \tab ValH12 \tab PropH12\cr
 #' }
 #'
-#' @param
-#'
-#' @param
-#'
-#' @return
-#'
 #' @export
 #'
+#' @param file_name Name of the file from which to read the data. May contain
+#'   "#NUM#" as a placeholder if you have multiple files (see plate_num).
+#'
+#' @param path The path to file (needs to end with "/").
+#'
+#' @param plate_num Number of the plate to read, inserted for "#NUM#".
+#'
+#' @param sep Separator used in the csv-file, either "," or ";" (see
+#'   \code{\link[utils]{read.csv}})
+#'
+#' @param cols Number of columns in the input matrix (\code{NULL} means
+#'   auto-detect).
+#'
+#' @param rows Number of rows containing values (not names / additional data)
+#'   in the input matrix (\code{NULL} means auto-detect).
+#'
+#' @param additional_vars Vector of strings containing the names for the
+#'   additional columns.
+#'
+#' @param additional_sep String / RegExp that separates additional vars, e.g.:
+#'   \code{"ID_blue_cold"} with \code{additional_sep = "_"} will be separated
+#'   into three columns containg \code{"ID"}, \code{"blue"} and \code{"cold"}.
+#'   If the separated data would exceed the columns in \code{additional_vars}
+#'   the last column will contain a string with separator (e.g.: "blue_cold").
+#'   If data is missing \code{NA} is inserted.
+#'
+#' @return A tibble containing (at minimum) \code{plate}, \code{position} and
+#' \code{value}.
+#'
 plate_read <- function(
-  plate_num,
+  file_name = "plate_#NUM#.csv",
   path = "./",
-  additional_vars = c(),
-  additional_sep = "[^[:alnum:]]+",
-  name_scheme = "plate_#NUM#.csv",
+  plate_num = 1,
   sep = ",",
   cols = 12,
-  rows = 8
+  rows = 8,
+  additional_vars = c(),
+  additional_sep = "[^[:alnum:]]+"
 ) {
   # make the pipe operator available to us
   `%>%` <- magrittr::`%>%`
 
-  file_name <- gsub(pattern = "#NUM#", replacement = plate_num, x = name_scheme)
+  # load file
+
+  file_name <- gsub(pattern = "#NUM#", replacement = plate_num, x = file_name)
   file_name <- paste0(path, file_name)
 
   if (!file.exists(file_name)) {
@@ -94,46 +119,118 @@ plate_read <- function(
       "and name_scheme (must contain \"#NUM#\")"))
   }
 
-  data_raw <- tibble::as_tibble(read.csv(
+  data_raw <- tibble::as_tibble(utils::read.csv(
     file = file_name,
     header = FALSE,
     sep = sep
   ))
 
+  # check dimenions of input
+
+  actual_cols <- length(data_raw)
+  actual_rows <- nrow(data_raw)
+  actual_vars <- length(additional_vars)
+
+  if (actual_vars == 0) {
+    additional_vars <- c("id")
+  }
+
+  if (is.null(cols)) {
+    # auto-detect
+    cols <- actual_cols
+  } else {
+    if (actual_cols < cols) {
+      stop(paste0(
+        "Column count in sheet (", actual_cols,
+        ") lower than expected (", cols,
+        "). Reducing cols to actual column count."))
+      cols <- actual_cols
+    } else if (actual_cols > cols) {
+      stop(paste0(
+        "Column count in sheet (", actual_cols,
+        ") larger than expected (", cols,
+        "). Ignoring residual columns."))
+    }
+  }
+
+  if (is.null(rows)) {
+    if (actual_vars == 0) {
+      # no names given
+      rows <- actual_rows
+    } else {
+      # there must be double the amount of rows than values
+      rows <- actual_rows / 2
+    }
+  } else {
+    # if there are names / additional variables double the amount of rows are
+    # needed
+    required_rows <- rows * ifelse(actual_vars > 0, 2, 1)
+
+    if (actual_rows < required_rows) {
+      stop(paste0(
+        "Row count in sheet (", actual_rows,
+        ") lower than required (", required_rows,
+        ")."))
+    } else if (actual_rows > required_rows) {
+      stop(paste0(
+        "Row count in sheet (", actual_rows,
+        ") larger than expected (", required_rows,
+        ")."))
+    }
+  }
+
+  # re-organise data
+
   data_tbl <- data_raw %>%
     dplyr::slice(1:rows)
 
-  names_tbl <- data_raw %>%
-    dplyr::slice((rows+1):(rows*2+1))
-
-  #return(names_tbl)
+  if (actual_vars > 0) {
+    # get the names if applicable
+    names_tbl <- data_raw %>%
+      dplyr::slice((rows+1):(rows*2+1))
+  }
 
   data_vec <- c()
   names_vec <- c()
+  positions_vec <- c()
 
   for (i in 1:cols) {
     column <- data_tbl %>%
       dplyr::pull(i) %>%
-      as.numeric(.)
+      as.numeric()
     data_vec <- c(data_vec, column)
 
-    column <- names_tbl %>%
-      dplyr::pull(i) %>%
-      as.character(.)
+    # determine position
+    if (rows < length(LETTERS)) {
+      column <- paste0(LETTERS[1:rows], i)
+    } else {
+      column <- paste0(seq(from = 1, to = rows, by = 1), "x", i)
+    }
+    positions_vec <- c(positions_vec, column)
+
+    if (actual_vars > 0) {
+      # use names
+      column <- names_tbl %>%
+        dplyr::pull(i) %>%
+        as.character()
+    } else {
+      # use position as name
+      column <- column
+    }
+
     names_vec <- c(names_vec, column)
   }
-
-  #return(names_vec)
 
   data <-
     tibble::tibble(
       plate = plate_num,
+      position = positions_vec,
       tmp = names_vec,
       value = data_vec
     ) %>%
     tidyr::separate(
-      col = tmp,
-      into = c("name", additional_vars),
+      col = "tmp",
+      into = additional_vars,
       sep = additional_sep,
       remove = TRUE,
       extra = "merge",
